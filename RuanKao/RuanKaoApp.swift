886d7c45f8a89439163255b0ed4d60f2a674a7d3
xiaobingtech <developer@xiaobingkj.com>
2025-12-04 21:36:00 +0800

    Update RuanKaoApp.swift

--- RuanKao/RuanKaoApp.swift
+++ RuanKao/RuanKaoApp.swift
@@ -13,8 +13,8 @@ import Firebase
 @main
 struct RuanKaoApp: App {
     init() {
-            Purchases.configure(withAPIKey: "test_YSwOwkZCyqduYrinjAeoiBUPiSW")
-        FirebaseApp.configure()
+//            Purchases.configure(withAPIKey: "test_YSwOwkZCyqduYrinjAeoiBUPiSW")
+            FirebaseApp.configure()
         }
     var body: some Scene {
         WindowGroup {
